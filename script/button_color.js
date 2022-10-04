663d7ae873ca841b798df0964d39f1b4fb65d0f1
ESCHAL-C <112564489+ESCHAL-C@users.noreply.github.com>
2022-10-04 12:01:31 -0500

    Add files via upload

--- script/button_color.js
+++ script/button_color.js
@@ -1,19 +1,19 @@
 $( document ).ready(function() {
   $( "#resetFormView" ).click(function() {
     $( "#resetFormView" ).css('background-color', 'DimGrey');
-    $( "#returnResultsLatLon" ).css('background-color', 'OrangeRed');
-    $( "#returnResultsRefMrkr" ).css('background-color', 'OrangeRed');
-    $( "#returnResultsDFO" ).css('background-color', 'OrangeRed');
-    $( "#returnResultsCS" ).css('background-color', 'OrangeRed');
-    $( "#addPointtoMiniMap_btn" ).css('background-color', 'PaleVioletRed');
+    $( "#returnResultsLatLon" ).css('background-color', '#EE3E42');
+    $( "#returnResultsRefMrkr" ).css('background-color', '#EE3E42');
+    $( "#returnResultsDFO" ).css('background-color', '#EE3E42');
+    $( "#returnResultsCS" ).css('background-color', '#EE3E42');
+    $( "#addPointtoMiniMap_btn" ).css('background-color', '#F08688');
   });
 });
 
 $( document ).ready(function() {
   $( "#returnResultsLatLon" ).click(function() {
-    $( "#returnResultsLatLon" ).css('background-color', 'green');
-    $( "#addPointtoMiniMap_btn" ).css('background-color', 'OrangeRed');
-    $( "#resetFormView" ).css('background-color', 'PaleVioletRed');
+    $( "#returnResultsLatLon" ).css('background-color', '#0053A0');
+    $( "#addPointtoMiniMap_btn" ).css('background-color', '#EE3E42');
+    $( "#resetFormView" ).css('background-color', '#F08688');
   });
 });
 
@@ -21,32 +21,32 @@ $( document ).ready(function() {
 
 $( document ).ready(function() {
   $( "#returnResultsRefMrkr" ).click(function() {
-    $( "#returnResultsRefMrkr" ).css('background-color', 'green');
-    $( "#addPointtoMiniMap_btn" ).css('background-color', 'OrangeRed');
-    $( "#resetFormView" ).css('background-color', 'PaleVioletRed');
+    $( "#returnResultsRefMrkr" ).css('background-color', '#0053A0');
+    $( "#addPointtoMiniMap_btn" ).css('background-color', '#EE3E42');
+    $( "#resetFormView" ).css('background-color', '#F08688');
   });
 });
 
 $( document ).ready(function() {
   $( "#returnResultsDFO" ).click(function() {
-    $( "#returnResultsDFO" ).css('background-color', 'green');
-    $( "#addPointtoMiniMap_btn" ).css('background-color', 'OrangeRed');
-    $( "#resetFormView" ).css('background-color', 'PaleVioletRed');
+    $( "#returnResultsDFO" ).css('background-color', '#0053A0');
+    $( "#addPointtoMiniMap_btn" ).css('background-color', '#EE3E42');
+    $( "#resetFormView" ).css('background-color', '#F08688');
   });
 });
 
 $( document ).ready(function() {
   $( "#returnResultsCS" ).click(function() {
-    $( "#returnResultsCS" ).css('background-color', 'green');
-    $( "#addPointtoMiniMap_btn" ).css('background-color', 'OrangeRed');
-    $( "#resetFormView" ).css('background-color', 'PaleVioletRed');
+    $( "#returnResultsCS" ).css('background-color', '#0053A0');
+    $( "#addPointtoMiniMap_btn" ).css('background-color', '#EE3E42');
+    $( "#resetFormView" ).css('background-color', '#F08688');
   });
 });
 
 $( document ).ready(function() {
   $( "#addPointtoMiniMap_btn" ).click(function() {
-    $( "#addPointtoMiniMap_btn" ).css('background-color', 'green');
-    $( "#resetFormView" ).css('background-color', 'OrangeRed');
+    $( "#addPointtoMiniMap_btn" ).css('background-color', '#0053A0');
+    $( "#resetFormView" ).css('background-color', '#EE3E42');
   });
 });
 
@@ -54,27 +54,27 @@ $( document ).ready(function() {
 
 $( document ).ready(function() {
   $( "#returnBDFO" ).click(function() {
-    $( "#returnBDFO" ).css('background-color', 'green');
-    $( "#returnEDFO" ).css('background-color', 'OrangeRed');
+    $( "#returnBDFO" ).css('background-color', '#0053A0');
+    $( "#returnEDFO" ).css('background-color', '#EE3E42');
   });
 });
 
 $( document ).ready(function() {
   $( "#returnEDFO" ).click(function() {
-    $( "#returnEDFO" ).css('background-color', 'green');
-    $( "#addOrAssembleQueryStr_btn" ).css('background-color', 'OrangeRed');
+    $( "#returnEDFO" ).css('background-color', '#0053A0');
+    $( "#addOrAssembleQueryStr_btn" ).css('background-color', '#EE3E42');
   });
 });
 
 $( document ).ready(function() {
   $( "#addOrAssembleQueryStr_btn" ).click(function() {
-    $( "#addOrAssembleQueryStr_btn" ).css('background-color', 'green');
-    $( "#mapProjects_btn" ).css('background-color', 'OrangeRed');
+    $( "#addOrAssembleQueryStr_btn" ).css('background-color', '#0053A0');
+    $( "#mapProjects_btn" ).css('background-color', '#EE3E42');
   });
 });
 
 $( document ).ready(function() {
   $( "#mapProjects_btn" ).click(function() {
-    $( "#mapProjects_btn" ).css('background-color', 'green');
+    $( "#mapProjects_btn" ).css('background-color', '#0053A0');
   });
 });
